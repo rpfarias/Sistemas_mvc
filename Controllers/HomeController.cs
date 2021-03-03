e0cfe98fbe0089e5e1cf0e081289bcd2fb73fbed
rpfarias <rpf1rafael@gmail.com>
2021-03-03 20:29:27 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Introducao.Models;
+using System.Web.Mvc;
+
+namespace Introducao.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var pessoa = new Pessoa
+            {
+                PessoaId = 1,
+                Nome = "Rafael Farias",
+                Tipo = "Administrador"
+            };
+            
+            //ViewData["PessoaId"] = pessoa.PessoaId;
+            //ViewBag.nome = pessoa.Nome;
+
+            return View(pessoa);
+        }   
+            
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Lista(Pessoa pessoa)//modelo via View Tipada
+        {
+            ViewData["PessoaId"] = pessoa.PessoaId;
+            ViewData["Nome"] = pessoa.Nome;
+            ViewData["Tipo"] = pessoa.Tipo;
+
+            return View(pessoa);
+        }
+    }
+}
